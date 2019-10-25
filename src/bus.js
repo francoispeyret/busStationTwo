80cbff518e943da7b463a52155ab313fb766ec29
François Peyret <fpeyret@axome.com>
2019-10-25 14:03:40 +0200

    [*] bus class

--- src/bus.js
+++ src/bus.js
@@ -1,5 +1,12 @@
-export class Bus () {
-    construct() {
+import * as THREE from "three";
 
+export default class Bus {
+    constructor() {
+        var busGeometry = new THREE.BoxGeometry( 1, 1, 3 );
+        var busMaterial = new THREE.MeshPhongMaterial( { color: 0xff0000 } );
+        this.mesh = new THREE.Mesh( busGeometry, busMaterial );
+        this.mesh.position.set( 0, .5, 2 );
+        this.mesh.castShadow = true; //default is false
+        this.mesh.receiveShadow = false; //default
     }
 }
